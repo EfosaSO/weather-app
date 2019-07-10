f10ddcb45f44e30ed4b419549af40133c3ee036f
Efosa Omorogbe <Efosa@TiToit.com>
2019-07-10 10:45:54 +0100

    Fixed bug of error results, was changing 'state' structure - Changed state back to object - Added results array to state - Refactored 'REMOVE_RESULT' action to accommodate changes Clearing weather input values after a search now for UX Overall code optimisation here

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,44 +5,46 @@ import SearchForm from "./SearchForm";
 import "./App.css";
 
 class App extends React.Component {
-	getWeather = e => {
-		const city = e.target.elements.city.value;
-		const country = e.target.elements.country.value;
-		e.preventDefault();
-		this.props.loadWeather(city, country);
-	};
+  getWeather = e => {
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+    this.props.loadWeather(city, country);
+    e.target.elements.city.value = null;
+    e.target.elements.country.value = null;
+  };
 
-	render() {
-		return (
-			<div className="app">
-				<header className="app-header">Weather</header>
-				<div className="wrapper">
-					<div className="main">
-						<SearchForm
-							loadWeather={this.getWeather}
-							error={this.props.weather.error}
-						/>
-						{/* Added a conditional to only map here if there are arrays in the object
+  render() {
+    return (
+      <div className="app">
+        <header className="app-header">Weather</header>
+        <div className="wrapper">
+          <div className="main">
+            <SearchForm
+              loadWeather={this.getWeather}
+              error={this.props.weather.error}
+            />
+            {/* Added a conditional to only map here if there are arrays in the object
             else it will produce a ".map undefined" error */}
-						{this.props.weather.length > 0
-							? this.props.weather.map((item, index) => {
-									if (item.city)
-										return (
-											<WeatherResult
-												id={index}
-												key={index}
-												temperature={item.temperature}
-												city={item.city}
-												country={item.country}
-												description={item.description}
-											/>
-										);
-							  })
-							: null}
-					</div>
-				</div>
-			</div>
-		);
-	}
+            {this.props.weather.results.length > 0
+              ? this.props.weather.results.map((item, index) => {
+                  if (item.city)
+                    return (
+                      <WeatherResult
+                        id={index}
+                        key={index}
+                        temperature={item.temperature}
+                        city={item.city}
+                        country={item.country}
+                        description={item.description}
+                      />
+                    );
+                })
+              : null}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 export default App;
--- src/reducers/weather.js
+++ src/reducers/weather.js
@@ -1,40 +1,55 @@
 import {
-	LOADING_SUCCESS,
-	LOADING_FAILURE,
-	REMOVE_RESULT,
-	EMPTY_SEARCH,
+  LOADING_SUCCESS,
+  LOADING_FAILURE,
+  REMOVE_RESULT,
+  EMPTY_SEARCH
 } from "../actions/weatherActionTypes";
 
-const initialState = [];
+const initialState = {
+  results: [],
+  error: null
+};
 
 export default function weather(state = initialState, action) {
-	switch (action.type) {
-		case LOADING_SUCCESS:
-			return [
-				...state,
-				{
-					temperature: action.payload.weather.main.temp,
-					city: action.payload.weather.name,
-					country: action.payload.weather.sys.country,
-					description: action.payload.weather.weather[0].description,
-				},
-			];
-		case REMOVE_RESULT:
-			return state.filter((weather, index) => {
-				return index !== action.payload.id;
-			});
+  switch (action.type) {
+    case LOADING_SUCCESS:
+      return {
+        ...state,
+        error: null, // Reset errors here
+        results: [
+          ...state.results,
+          {
+            temperature: action.payload.weather.main.temp,
+            city: action.payload.weather.name,
+            country: action.payload.weather.sys.country,
+            description: action.payload.weather.weather[0].description
+          }
+        ]
+      };
+    case REMOVE_RESULT:
+		// Refactored, was returning state before and changing object structure 
+      state.results = state.results.filter((weather, index) => {
+        return index !== action.payload.key;
+      });
+      return {
+        ...state
+      };
 
-		case LOADING_FAILURE:
-			return {
-				error: `Can't find ${action.payload.error.city}, ${
-					action.payload.error.country
-				}`,
-			};
+    case LOADING_FAILURE:
+      return {
+		  // Spreading state here to keep existing valid items for UX improvements
+        ...state,
+        error: `Can't find ${action.payload.error.city}, ${
+          action.payload.error.country
+        }`
+      };
 
-			case EMPTY_SEARCH:
-				return action.payload.error;
-				
-		default:
-			return state;
-	}
+    case EMPTY_SEARCH:
+      return {
+        error: action.payload.error
+      };
+
+    default:
+      return state;
+  }
 }
